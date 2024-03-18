c2525dfc04f58d4b121d611a98de2cdadeb30c11
ArhanBaltes <arhangamebang@gmail.com>
2024-03-18 12:48:30 +0100

    Old Commit that failed to resolve

--- src/main/java/GameLoad.java
+++ src/main/java/GameLoad.java
@@ -3,38 +3,13 @@ package src.main.java;
 import src.main.java.Entities.Hostile.Enemies.Enemy;
 import src.main.java.PlayingCharacter.Player;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 // The point of this class is to set up the game. It also eliminates code from psvm
 public class GameLoad {
     public static void StartGame() {
         Scanner comm = new Scanner(System.in);
-
-        try {
-            FileReader reader = new FileReader("src/main/java/test.txt");
-            reader.close();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            String pathName = new File(".").getCanonicalPath();
-            File textFile = new File(pathName, "/src/main/java/test.txt");
-            System.out.println(textFile);
-            System.out.println(textFile.exists());
-            System.out.println(pathName);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        // FileReader reader = new FileReader(absolPath);
-
         Player roald = new Player();
         Enemy beast = new Enemy(20, 1, 5);
 
